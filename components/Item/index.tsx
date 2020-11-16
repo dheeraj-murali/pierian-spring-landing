cc49b429dd9e7a11ac8200da23c46a1531be0225
Dheeraj <410dheeraj@gmail.com>
2020-11-16 22:31:04 +0530

    updated animations

--- components/Item/index.tsx
+++ components/Item/index.tsx
@@ -6,32 +6,31 @@ import { useInView } from 'react-intersection-observer';
 export const Item = (prop: ItemProps) => {
 	const { revert, image, subtitle, title } = prop;
 	const [ref, inView] = useInView({
-		rootMargin: '-15%',
+		rootMargin: '-20%',
 		triggerOnce: true,
 	});
 
 	return (
 		<Box ref={ref} w='full'>
-			<ScaleFade initialScale={0.9} in={inView}>
-				<Flex
-					alignItems='center'
-					justifyContent='space-evenly'
-					flexDir={revert ? 'row-reverse' : 'row'}
-					flexWrap='wrap-reverse'
-					w='full'
-					my='10'
-					px={{ xl: '10' }}
+			<Flex
+				alignItems='center'
+				justifyContent='space-evenly'
+				flexDir={revert ? 'row-reverse' : 'row'}
+				flexWrap='wrap-reverse'
+				w='full'
+				my='10'
+				px={{ xl: '10' }}
+			>
+				<Box
+					w={{ base: 'xs', sm: 'sm', xl: 'lg' }}
+					textAlign={{ base: 'center', md: 'left' }}
+					p='5'
 				>
-					<Box
-						w={{ base: 'xs', sm: 'sm', xl: 'lg' }}
-						textAlign={{ base: 'center', md: 'left' }}
-						p='5'
-					>
-						<Heading size='lg'>{title}</Heading>
-						<Divider border='2px' my='5' />
-						<Text>{subtitle}</Text>
-					</Box>
-
+					<Heading size='lg'>{title}</Heading>
+					<Divider border='2px' my='5' />
+					<Text>{subtitle}</Text>
+				</Box>
+				<ScaleFade initialScale={0.9} in={inView}>
 					<Box
 						w={{ base: 'xs', md: 'xs', lg: 'md' }}
 						h={{ base: 'xs', md: 'xs', lg: 'sm' }}
@@ -46,8 +45,8 @@ export const Item = (prop: ItemProps) => {
 							layout='responsive'
 						/>
 					</Box>
-				</Flex>
-			</ScaleFade>
+				</ScaleFade>
+			</Flex>
 		</Box>
 	);
 };
